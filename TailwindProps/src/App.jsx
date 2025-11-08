b9997e2ffbf9ffbc9128a324304699ca43fd4099
JASH MANDALIYA <167299436+Jashmandaliya18@users.noreply.github.com>
2025-11-08 16:47:38 +0530

    Add Card component and integrate it into App

--- TailwindProps/src/App.jsx
+++ TailwindProps/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Card from './components/Card.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
   return (
     <>
       <h1 className='bg-green-500 p-4 rounded-xl text-black font-medium'>Tailwind Test</h1>
+      <Card />
+      <Card />
     </>
   )
 }
---
+++ TailwindProps/src/components/Card.jsx
@@ -0,0 +1,21 @@
+
+function Card() {
+    return (
+        <div className="flex flex-col items-center p-7 rounded-2xl">
+            <div>
+                <img className="size-48 shadow-xl rounded-md" alt="" src="https://images.pexels.com/photos/3284268/pexels-photo-3284268.jpeg" />
+            </div>
+            <div className="flex">
+                <span className="text-2xl f">Class Warfare</span>
+                <span>The Anti-Patterns</span>
+                <span className="flex">
+                    <span>No. 4</span>
+                    <span>·</span>
+                    <span>2025</span>
+                </span>
+            </div>
+        </div >
+    )
+}
+
+export default Card
